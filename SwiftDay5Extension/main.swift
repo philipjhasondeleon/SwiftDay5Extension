4a1f0610efbd85b6324be41b23a3fe5bf55a77ba
philip <jhasondeleon@gmail.com>
2020-02-12 19:05:34 -0500

    Initial Commit

---
+++ SwiftDay5Extension/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftDay5Extension
+//
+//  Created by MacStudent on 2020-02-12.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
