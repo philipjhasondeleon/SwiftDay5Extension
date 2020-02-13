2a1b8dc69549eae338f1e3ec6cacffb4c36cd0f4
philip <jhasondeleon@gmail.com>
2020-02-12 19:58:37 -0500

    initial commit

--- SwiftDay5Extension/main.swift
+++ SwiftDay5Extension/main.swift
@@ -8,5 +8,38 @@
 
 import Foundation
 
-print("Hello, World!")
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
+
+    func currency() -> String
+    {
+        let s = String.init(format: "$%.2f", self)      //change currency
+        return s
+    }
+    
+    
+}
+
+
+var meter = 100.5
+print(meter.km)
+print(meter.m)
+print(meter.currency())                                 //change currency
+
+
+extension String
+{
+    func repeatNTimes(n:Int)
+    {
+        for _ in 1...n
+        {
+            print(self)
+        }
+    }
+}
+"philip".repeatNTimes(n: 10)
 
